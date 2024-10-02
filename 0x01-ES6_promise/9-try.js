c95272861149ec0bd06b53e6ded6fa2101b64c44
henokgeta12 <henokasfaw47@icloud.com>
2024-10-02 10:27:50 -0700

    alx backend

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,15 @@
+//
+rt default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
+// Now, the longer 
