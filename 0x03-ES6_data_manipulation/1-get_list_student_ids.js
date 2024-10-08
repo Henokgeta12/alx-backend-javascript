e62229488bdd7a9634a1cb232c43bdff7a0a3870
Henokgeta12 <henokasfaw47@icloud.com>
2024-10-08 12:57:19 -0700

    remove error

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,14 +1,14 @@
 /**
  * Retrieves ids from a list of students.
  * @param {{
-*   id: Number,
-*   firstName: String,
-*   location: String
+*   id: number,
+*   firstName: string,
+*   location: string
 * }[]} students - The list of students.
-* @returns
+* @returns {number[]} An array of student ids.
 */
 export default function getListStudentIds(students) {
- if (students instanceof Array) {
+ if (Array.isArray(students)) {
    return students.map((student) => student.id);
  }
  return [];
