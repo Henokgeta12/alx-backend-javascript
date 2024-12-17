278435e399b3356bfe1a68e23e670f699abf08f5
Henokgeta12 <henokasfaw47@icloud.com>
2024-12-17 20:17:50 +0300

    add extension

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,9 @@
 export default function getStudentsByLocation(students, city) {
     return students.filter((student) => student.location === city);
   }
+
+  [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ]
